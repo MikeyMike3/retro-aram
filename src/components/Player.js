9d755554fd785e91515002c2f8b02261b6dc3af2
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-04-06 03:58:42 -0700

    Formatted Win Rate

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 export const Player = (props) => {
+	const formatWinRate = (winRate) => {
+		const decimalPlaces = winRate.toString().split(".")[1];
+		return decimalPlaces && decimalPlaces.length > 2
+			? parseFloat(winRate).toFixed(2)
+			: winRate;
+	};
 	return (
 		<div>
 			<h4>{props.name}</h4>
 			<p>Wins: {props.wins}</p>
 			<p>Losses: {props.losses}</p>
 			<p>Games Played: {props.totalGamesPlayed}</p>
-			<p>Win Rate: {props.winRate.toFixed(2)}%</p>
+			<p>Win Rate: {formatWinRate(props.winRate)}%</p>
 			<p>MMR: {props.mmr}</p>
 			<br></br>
 			<br></br>
