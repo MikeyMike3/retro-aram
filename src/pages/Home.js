20fd548a98cd2bf2475ed852afd1666a3918e17c
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-04-08 16:32:26 -0700

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,7 +13,7 @@ export const Home = () => {
 				<p className="description">
 					This website is for all things{" "}
 					<span className="aram-span">Aram</span>. We got our own
-					ranked leaderboard with more features to come, Stay tuned.
+					ranked leaderboard with more features to come. Stay tuned.
 				</p>
 			</div>
 		</div>
