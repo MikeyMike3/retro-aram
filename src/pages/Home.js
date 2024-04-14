225e2ab1e10cc74689c6e2ba73970efce4fc5fe5
MikeyMike3 <michaelmorgan19103@gmail.com>
2024-04-13 20:39:51 -0700

    Added back the background video

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,22 +2,31 @@ import React from "react";
 
 export const Home = () => {
 	return (
-		<div className="wrapper">
-			<h1 className="welcome glass">
-				Welcome to the <span className="retro-span">Retro </span>
-				<span className="future-span">Future </span>
-				<span className="aram-span">Aram </span> website
-			</h1>
+		<>
+			<video
+				src="video/retro-aram-video-1.mp4"
+				autoPlay
+				loop
+				muted
+				id="bgVideo"
+			></video>
+			<div className="wrapper">
+				<h1 className="welcome glass">
+					Welcome to the <span className="retro-span">Retro </span>
+					<span className="future-span">Future </span>
+					<span className="aram-span">Aram </span> website
+				</h1>
 
-			<div className="description-container glass">
-				<p className="description">
-					This website is for all things{" "}
-					<span className="aram-span">Aram</span>. We have our own
-					Ranked Leaderboard, Tier List, Team Maker and Ultimate{" "}
-					<span className="aram-span">Aram</span> mode with more
-					features to come. Stay tuned.
-				</p>
+				<div className="description-container glass">
+					<p className="description">
+						This website is for all things{" "}
+						<span className="aram-span">Aram</span>. We have our own
+						Ranked Leaderboard, Tier List, Team Maker and Ultimate{" "}
+						<span className="aram-span">Aram</span> mode with more
+						features to come. Stay tuned.
+					</p>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 };
